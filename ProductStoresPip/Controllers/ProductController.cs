58523e744739ea6eca5ed137079f874f9ef320cc
VS65477 <VS65477@APPW46ZLPC1102>
2022-07-11 10:36:17 +0530

    Add project files.

---
+++ ProductStoresPip/Controllers/ProductController.cs
@@ -0,0 +1,137 @@
+﻿using ProductStoresPip.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductStoresPip.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        private ProductStoreEntities db=new ProductStoreEntities();
+        public ActionResult Dashboard()
+        {
+            try
+            {
+                return View(db.Products.ToList());
+            }
+            catch (Exception ex)
+            {
+                return View(ex.Message, "error");
+            }
+        }
+        public ActionResult Details(int? id)
+        {
+            return View(db.Products.Where(x=>x.id==id).FirstOrDefault());
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Product pro)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Products.Add(pro);
+                    db.SaveChanges();
+
+                }
+                return RedirectToAction("Dashboard");
+            }
+            catch (Exception ex)
+            {
+                return View(ex.Message);
+            }
+        }
+        public ActionResult Edit(int? id)
+        {
+            if(id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var data=db.Products.SingleOrDefault(x => x.id == id);
+            if(data==null)
+            {
+                return HttpNotFound();
+            }
+            return View(data);
+
+          
+        }
+       [HttpPost]
+       public ActionResult Edit(Product product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(product).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Dashboard");
+                }
+                return View(product);
+            }
+           catch(Exception ex)
+            {
+                return View(ex.Message, "Error");
+            }
+            
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var data = db.Products.SingleOrDefault(x => x.id == id);
+            if (data == null)
+            {
+                return HttpNotFound();
+            }
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                Product product = db.Products.Find(id);
+                db.Products.Remove(product);
+                db.SaveChanges();
+                return RedirectToAction("Dashboard");
+            }
+            catch (Exception ex)
+            {
+                return View(ex.Message,"Error");
+            }
+            
+           
+        }
+        public ActionResult Search(string search)
+        {
+            try
+            {
+                if (search==null)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    var data = db.Products.Where(x => x.Product1.Contains(search)).ToList();
+                    return View(data);
+                }
+            }
+            catch(Exception ex)
+            {
+                return View(ex.Message, "error");
+            }
+        }
+    }
+}
---
+++ ProductStoresPip/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ProductStoresPip")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProductStoresPip")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("23a97681-39f0-447e-8409-6a5656ae56e3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
